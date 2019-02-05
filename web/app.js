61ac6d45426efd7406aaa186e583b0edbb94e339
Vivek Kumar Pandey <vivekp@thoughtworks.com>
2019-02-05 15:28:03 +0530

    adds endpoints to get and save the numbers

--- web/app.js
+++ web/app.js
@@ -1,10 +1,29 @@
 const express = require('express');
+const {Client} = require('pg');
+const db = require('./dbHandler.js');
+
 const app = express();
+
+const connectionString = process.env.DATABASE_URL || 'postgres://127.0.0.1:5432/samaj';
+
+const client = new Client(connectionString);
+client.connect();
+
+app.client = client;
 
 const port = 8000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(express.urlencoded({
+  extended: false
+}));
 
-app.post('/', (req, res) => res.send('Hello World!'));
+app.get('/numbers', (req, res) => {
+  db.getNumbers(req, res);
+});
+
+
+app.post('/number', (req, res) => {
+  db.saveData(req, res);
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ web/dbHandler.js
@@ -0,0 +1,26 @@
+const saveData = (req, res) => {
+  let client = req.app.client;
+
+  client.query(`create table numbers (number int )`, (err, data) => {
+    client.query(`insert into numbers values($1)`, [req.body.number]);
+    res.send();
+  });
+};
+
+
+const getNumbers = (req, res) => {
+  let client = req.app.client;
+
+  client.query(`select number from numbers`, (err, data) => {
+    if (err) {
+      res.send([]);
+      return;
+    }
+    res.send(data.rows);
+  });
+};
+
+module.exports = {
+  saveData,
+  getNumbers
+};
