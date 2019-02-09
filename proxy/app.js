5074882778ab1d091fd4b46823fbaecf21038e47
Vivek Kumar Pandey <vivekp@thoughtworks.com>
2019-02-09 12:59:29 +0530

    proxy gets random server from two web servers

--- proxy/app.js
+++ proxy/app.js
@@ -6,12 +6,20 @@ const app = express();
 const port = 5000;
 
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  console.log('Time:', Date.now())
+  let dnsServer = process.env.WEB_DNS.split(',');
+  req.domain = dnsServer[Math.floor(Math.random() * 2)];
+  next();
+})
+
 app.all('*', (req, res) => {
   let options = {
     method: req.method,
     body: req.body,
     json: true,
-    uri: `${process.env.WEB1_DNS}${req.url}`
+    uri: `${req.domain}${req.url}`
   };
 
   console.log('request->', options);
